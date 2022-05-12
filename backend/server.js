c4e1c2df169a261a9405c8700545fbe4689654d7
Kaltrina Gashi <kaltrinag54@gmail.com>
2022-05-13 01:51:16 +0200

    Create Sample Products In MongoDB

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 const app = express();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/shop-ks',{
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/shop-ks', {
     // useNewUrlParser: true,
     // useUnifiedTopology: true,
     // useCreateIndex: true,
@@ -16,10 +16,11 @@ app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
-app.use((err,req,res,next) =>{
-    res.status(500).send({message:err.message});
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
-});
+}
+);
